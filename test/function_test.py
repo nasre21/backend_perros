c2befb690d615bc6121d26e0ab9b75bc52ac0d32
nasre21 <nasou2001@gmail.com>
2023-07-22 12:11:33 +0200

    trabajo completo

---
+++ test/function_test.py
@@ -0,0 +1,48 @@
+
+import pytest
+from routes.functions import * 
+from flask import Flask
+import os
+from src.webserver import create_app
+import mysql.connector 
+
+
+
+
+
+@pytest.fixture
+def app():
+        
+    app = create_app("Dogs_product")
+    
+    app.config['TESTING'] = True
+    return app
+
+
+def test_create_user(app):
+        with app.test_client() as client:
+            response = client.get('/user')
+            assert response.status_code == 200
+
+import pytest
+import mysql.connector
+from mysql.connector import errors
+
+import pytest
+import mysql.connector
+
+def test_exist_database():
+    try:
+        con = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="NASSERDINE",
+            database="Dogs_product",
+            port=3306
+        )
+        # If the connection is successful, the test passes
+        assert con.is_connected()
+        con.close()  # Close the connection to avoid leaving connections open
+    except Exception as e:
+        # If an exception occurs, fail the test with the error message
+        pytest.fail(f"Caught exception: {type(e).__name__}")
